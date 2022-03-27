a77b1a2b18a881e0b501901f1b159148f2eca2b9
Daniel Torres <danni.torres621@gmail.com>
2022-03-27 15:47:46 -0500

    Changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,16 +1,15 @@
 export const environment = {
   production: true,
   url : 'https://identitytoolkit.googleapis.com/v1/accounts:',
-  apiKey : 'AIzaSyDXxIMlFIWXeCKN2CssMm6aTYLVD1K0R70',
-  urlusers : 'https://gluky-app-default-rtdb.firebaseio.com/',
+  apiKey : 'AIzaSyAYX-WFse4tv8zME5szgFihSpuBT3rC3-k',
+  urlusers : 'https://gluky-app-prueba-default-rtdb.firebaseio.com/',
   claveCryto : 'Mesofilos39085423$&',
   firebaseConfig : {
-    apiKey: "AIzaSyDXxIMlFIWXeCKN2CssMm6aTYLVD1K0R70",
-    authDomain: "gluky-app.firebaseapp.com",
-    databaseURL: "https://gluky-app-default-rtdb.firebaseio.com",
-    projectId: "gluky-app",
-    storageBucket: "gluky-app.appspot.com",
-    messagingSenderId: "355863303434",
-    appId: "1:355863303434:web:de0ceda0616262e3d016f3"
+    apiKey: "AIzaSyAYX-WFse4tv8zME5szgFihSpuBT3rC3-k",
+    authDomain: "gluky-app-prueba.firebaseapp.com",
+    projectId: "gluky-app-prueba",
+    storageBucket: "gluky-app-prueba.appspot.com",
+    messagingSenderId: "373312289126",
+    appId: "1:373312289126:web:22f1061a65213d2e70b0a7"
   }
 };
